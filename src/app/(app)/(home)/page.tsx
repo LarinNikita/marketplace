dd8702a1527fe319778adb40a5c649dfe16d3366
Larin Nikita <l_nikita2000@mail.ru>
2025-05-08 17:46:35 +0700

    Enhance Categories collection. Add subcategories, color, slig, etc.

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -8,6 +8,12 @@ export default async function Home() {
 
 	const data = await payload.find({
 		collection: 'categories',
+		depth: 1, // Populate subcategories
+		where: {
+			parent: {
+				exists: false,
+			},
+		},
 	});
 
 	return <pre>{JSON.stringify(data, null, 4)}</pre>;
