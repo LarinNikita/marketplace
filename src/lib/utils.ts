82f5aabaa8380aefd34ee00ee49945ab7b3644df
Larin Nikita <l_nikita2000@mail.ru>
2025-05-25 16:24:45 +0700

    Add middleware logic to rewrite tenants route to subdomain. Modify generateTenantURL method. Review all stripe urls

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,17 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-// TODO Add subdomain in url
 export function generateTenantUrl(tenantSlug: string) {
-	return `/tenants/${tenantSlug}`;
+	// In dev mode, use normal routing
+	if (process.env.NODE_ENV === 'development') {
+		return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
+	}
+
+	const protocol = 'https';
+	const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
+
+	// In prod, use subdomain routing
+	return `${protocol}://${tenantSlug}.${domain}`;
 }
 
 export function formatCurrency(value: number | string) {
